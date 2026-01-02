7185f2b0bf2416b12ecb945718c21d4774106811
Minh Tran <minhtr94@gmail.comm>
2026-01-02 21:48:37 +0000

    more work on the function notebook as well as a better convert_notebook.py script

--- convert_notebook.py
+++ convert_notebook.py
@@ -6,21 +6,25 @@ import os
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(message)s")
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-l", "--report_name_list", nargs="+", help="<Required> Set flag", required=True
-)
+
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-n", "--report_name")
+group.add_argument("-l", "--report_name_list", nargs="+")
+
 args = parser.parse_args()
 
+if args.report_name:
+    args.report_name_list = [args.report_name]
 
 for report_name in args.report_name_list:
     path = r"assets\notebooks"
     notebook = rf"{path}\{report_name}.ipynb"
-    html = rf"{path}_html"
-    output_dir = rf"--output-dir='{html}'"
+    html_dir = rf"{path}_html"
+    output_dir = rf"--output-dir='{html_dir}'"
     logging.info(f"Begin converting to html for {notebook}")
     command = (
         f"jupyter nbconvert {output_dir} --to html --no-input --no-prompt {notebook}  "
     )
     logging.info(f"Running command: {command}")
     os.system(command)
-    logging.info(f"Finish converting notebook to {html}")
+    logging.info(f"Finish converting notebook to {html_dir}")
